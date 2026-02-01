67324e9f32ccbb8ab68151371993124dff42adc3
CoderAdonis313 <coderadonis@gmail.com>
2026-01-31 23:24:21 -0500

    savings changes

--- run_commands.sh
+++ run_commands.sh
@@ -1,21 +1,21 @@
 echo "Deleting outputs & visualizations folders"
-rm -rf local_data/examples/fractals/outputs
-rm -rf local_data/examples/fractals/visualizations
+rm -rf local_data/examples/baygon/outputs
+rm -rf local_data/examples/baygon/visualizations
 
 export PYTHONDONTWRITEBYTECODE=1
 echo "Drawing input coordinates"
-python -m megapose.scripts.run_inference_on_example fractals --vis-detections
+python -m megapose.scripts.run_inference_on_example baygon --vis-detections
 echo "Successful input coordinates"
 
 sleep 1
 
 echo "Running inference" 
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
-python -m megapose.scripts.run_inference_on_example fractals --run-inference --model megapose-1.0-RGB
+python -m megapose.scripts.run_inference_on_example baygon --run-inference --model megapose-1.0-RGB
 echo "Successful inference"
 
 sleep 1
 
 echo "Drawing visualizations"
-python -m megapose.scripts.run_inference_on_example fractals --vis-outputs
+python -m megapose.scripts.run_inference_on_example baygon --vis-outputs
 echo "Successful Visualizations"
